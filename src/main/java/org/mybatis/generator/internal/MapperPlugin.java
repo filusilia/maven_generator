1c967d0366002a32a444ea1c2b70e4ec45d31c92
alice <459666001@qq.com>
2018-01-04 10:57:37 +0800

    添加通用mapper所需要的自定义mapper配置 重写了生成类

---
+++ src/main/java/org/mybatis/generator/internal/MapperPlugin.java
@@ -0,0 +1,50 @@
+package org.mybatis.generator.internal;
+
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import java.util.List;
+
+/**
+ * @author alice on 2017/6/29 0029.
+ * @version 1.0
+ * @since 1.0
+ */
+public class MapperPlugin extends PluginAdapter {
+    /**
+     * This method is called after all the setXXX methods are called, but before
+     * any other method is called. This allows the plugin to determine whether
+     * it can run or not. For example, if the plugin requires certain properties
+     * to be set, and the properties are not set, then the plugin is invalid and
+     * will not run.
+     *
+     * @param warnings add strings to this list to specify warnings. For example, if
+     *                 the plugin is invalid, you should specify why. Warnings are
+     *                 reported to users after the completion of the run.
+     * @return true if the plugin is in a valid state. Invalid plugins will not
+     * be called
+     */
+    @Override
+    public boolean validate(List<String> warnings) {
+        return false;
+    }
+
+    @Override
+    public boolean clientGenerated(Interface interfaze, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        //获取实体类
+        FullyQualifiedJavaType entityType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
+        //import接口
+//        for (String mapper : mappers) {
+//            interfaze.addImportedType(new FullyQualifiedJavaType(mapper));
+//            interfaze.addSuperInterface(new FullyQualifiedJavaType(mapper + "<" + entityType.getShortName() + ">"));
+//        }
+        //import实体类
+        interfaze.addImportedType(entityType);
+        return true;
+    }
+}
+
+
